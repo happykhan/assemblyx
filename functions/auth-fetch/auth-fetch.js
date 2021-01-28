fa7fa99ede15ebc3ace8ec3724a95616a58b68b8
Nabil-Fareed Alikhan <blackjack69n@gmail.com>
2021-01-28 17:07:13 +0000

    serverless

--- functions/auth-fetch/auth-fetch.js
+++ functions/auth-fetch/auth-fetch.js
@@ -2,19 +2,11 @@
 
 const fetch = require('node-fetch')
 
-const handler = async function (event, context) {
+const handler = async function (event, context, callback) {
   try {
     const response = await fetch('http://rest.pubmlst.org/db/pubmlst_rmlst_seqdef_kiosk/schemes/1/sequence', JSON.stringify(event.payload))
-    if (!response.ok) {
-      // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.statusText }
-    }
-    const data = await response.json()
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ identity, user, msg: data.value }),
-    }
+
   } catch (error) {
     // output to netlify function log
     console.log(error)
@@ -24,6 +16,12 @@ const handler = async function (event, context) {
       body: JSON.stringify({ msg: error.message }),
     }
   }
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      result: response
+    })
+  });  
 }
 
 module.exports = { handler }
