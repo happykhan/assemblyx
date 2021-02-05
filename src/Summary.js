3c6d600f2085ca1ab9ccd2dbd08f74b0db9fd67f
Nabil-Fareed Alikhan <blackjack69n@gmail.com>
2021-02-05 12:15:46 +0000

    heroku changes

--- src/Summary.js
+++ src/Summary.js
@@ -1,36 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
 import { useTable, useRowSelect } from 'react-table'
 import AssemblyChart from './AssemblyChart'
 
-const Styles = styled.div`
-  padding: 1rem;
-
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-`
 
 const IndeterminateCheckbox = React.forwardRef(
   ({ indeterminate, ...rest }, ref) => {
